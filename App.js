8aef2dcefea01f1b9a6977b57cd0c16ac12b76dd
Biven <biven@lougeek.com>
2019-05-02 10:14:13 -0400

    Add spinner to login page as component and add log out to the app.

--- App.js
+++ App.js
@@ -1,26 +1,54 @@
 import firebase from 'firebase'
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import  { Header } from './src/components'
+import {View} from 'react-native';
+import {Button, Card, CardSection, Header, Spinner} from './src/components/common'
+import LoginForm from './src/components/LoginForm';
 
-export default class App extends Component{
+export default class App extends Component {
+    state = {loggedIn: null};
+
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: "AIzaSyDGv7HqpizLqjhMwXuSPR4MFoOvp0TMmA8",
-            authDomain: "authentication-2a68b.firebaseapp.com",
-            databaseURL: "https://authentication-2a68b.firebaseio.com",
-            projectId: "authentication-2a68b",
-            storageBucket: "authentication-2a68b.appspot.com",
-            messagingSenderId: "59454858875"
+            apiKey: 'AIzaSyDGv7HqpizLqjhMwXuSPR4MFoOvp0TMmA8',
+            authDomain: 'authentication-2a68b.firebaseapp.com',
+            databaseURL: 'https://authentication-2a68b.firebaseio.com',
+            projectId: 'authentication-2a68b',
+            storageBucket: 'authentication-2a68b.appspot.com',
+            messagingSenderId: '59454858875'
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({loggedIn: true});
+            } else {
+                this.setState({loggedIn: false});
+            }
         });
     }
 
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button onPress={()=>{console.log("SigningOut!");firebase.auth().signOut();}}>Log out</Button>
+                        </CardSection>
+                    </Card>
+                );
+            case false:
+                return <LoginForm/>;
+            default:
+                return <Spinner size={'large'}/>;
+        }
+    }
+
     render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-        <Text>An App</Text>
-      </View>
-    );
-  }
+        return (
+            <View>
+                <Header headerText='Authentication'/>
+                {this.renderContent()}
+            </View>
+        );
+    }
 }
